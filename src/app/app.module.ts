93bf180e77f6b835a36175125fba3040d1a600af
klukv <tema200260@gmail.com>
2023-06-06 23:35:33 +0300

    создание проекта и начальная верстка header и блока с личной страницой

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SearchComponent } from './components/search/search.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { AppRoutingModule } from './app-routing.module';
+import { MyPageComponent } from './components/my-page/my-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    HeaderComponent,
+    SearchComponent,
+    MainPageComponent,
+    MyPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    container: {
+      center: true,
+      padding: '5px',
+      screens: {
+        sm: '600px',
+        md: '728px',
+        lg: '984px',
+        xl: '1240px',
+        '2xl': '1360px',
+      }
+    },
+    extend: {},
+  },
+  plugins: [],
+};
