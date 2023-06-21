8cdc4f38a38282c45fbd951dbe622a1181ba2a8f
klukv <86426111+klukv@users.noreply.github.com>
2023-06-21 14:25:39 +0300

    добавил автофокус при клике по иконке комменатриев

--- src/app/components/post-block/post-block.component.ts
+++ src/app/components/post-block/post-block.component.ts
@@ -1,12 +1,19 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ElementRef, ViewChild } from '@angular/core';
 import { IPost } from 'src/app/models/post';
 
 @Component({
   selector: 'app-post-block',
   templateUrl: './post-block.component.html',
-  styleUrls: ['./post-block.component.css']
+  styleUrls: ['./post-block.component.css'],
 })
-export class PostBlockComponent {
+export class PostBlockComponent{
   @Input() post: IPost;
+  @ViewChild('inputComment') inputComment: ElementRef;
+
+  constructor() {}
+  
+  clickIconComment() {
+    this.inputComment.nativeElement.focus();
+  }
 
 }
