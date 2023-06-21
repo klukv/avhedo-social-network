2a142cda5391238e671ef4a5526d63e17aa3b8b0
klukv <86426111+klukv@users.noreply.github.com>
2023-06-21 12:30:48 +0300

    создал фейковые данные (data)

---
+++ src/app/data/friendsData.ts
@@ -0,0 +1,62 @@
+export const friendsData = [
+    {
+        id: 0,
+        username: 'Данил',
+        age: 21,
+      },
+      {
+        id: 1,
+        username: 'Максим',
+        age: 25,
+      },
+      {
+        id: 2,
+        username: 'Петя',
+        age: 16,
+      },
+      {
+        id: 3,
+        username: 'Лера',
+        age: 19,
+      },
+      {
+        id: 4,
+        username: 'Татьяна',
+        age: 25,
+      },
+      {
+        id: 5,
+        username: 'Юля',
+        age: 20,
+      },
+      {
+        id: 6,
+        username: 'Никита',
+        age: 21,
+      },
+      {
+        id: 7,
+        username: 'Яна',
+        age: 25,
+      },
+      {
+        id: 8,
+        username: 'Иван',
+        age: 16,
+      },
+      {
+        id: 9,
+        username: 'Глеб',
+        age: 19,
+      },
+      {
+        id: 10,
+        username: 'Даша',
+        age: 25,
+      },
+      {
+        id: 11,
+        username: 'Григорий',
+        age: 20,
+      },
+] 
--- src/app/services/friends.service.ts
+++ src/app/services/friends.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { IFriends } from '../models/friends';
-import { BehaviorSubject, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,36 +7,7 @@ import { BehaviorSubject, Subject } from 'rxjs';
 export class FriendsService {
 
   friendsList: IFriends[] = [
-    {
-      id: 0,
-      username: 'Данил',
-      age: 21,
-    },
-    {
-      id: 1,
-      username: 'Максим',
-      age: 25,
-    },
-    {
-      id: 2,
-      username: 'Петя',
-      age: 16,
-    },
-    {
-      id: 3,
-      username: 'Лера',
-      age: 19,
-    },
-    {
-      id: 4,
-      username: 'Татьяна',
-      age: 25,
-    },
-    {
-      id: 5,
-      username: 'Юля',
-      age: 20,
-    },
+   
   ];
 
   constructor() { }
