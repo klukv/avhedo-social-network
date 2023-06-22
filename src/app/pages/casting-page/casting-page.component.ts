6323def17906cf12994ebf0e427c3b20b7b4812b
klukv <86426111+klukv@users.noreply.github.com>
2023-06-22 15:06:54 +0300

    реализация отображения карточке выбора при первом заходе на страницу знакомств

--- src/app/pages/casting-page/casting-page.component.ts
+++ src/app/pages/casting-page/casting-page.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-casting-page',
   templateUrl: './casting-page.component.html',
-  styleUrls: ['./casting-page.component.css']
+  styleUrls: ['./casting-page.component.css'],
 })
 export class CastingPageComponent {
+  isClickCard: boolean;
+  selectLinks = ['/likes', '/contacts'];
 
+  constructor(private activeRoute: Router) {}
+  ngDoCheck() {
+    this.isClickCard = this.selectLinks.some((link) =>
+      this.activeRoute.url.includes(link)
+    );
+  }
 }
