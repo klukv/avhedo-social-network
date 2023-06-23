5350d53fd4a3e6930cf59b180290ab3943ffeedd
klukv <86426111+klukv@users.noreply.github.com>
2023-06-23 13:28:55 +0300

    создание поиска на стороне клиента

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { IFriends } from 'src/app/models/friends';
+import { FriendsService } from 'src/app/services/friends.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
+  private _searchFriends: IFriends[] = [];
 
+  constructor(private friendsService: FriendsService) {}
+
+  filterSelectFriend(usernameFriend: string) {
+    this._searchFriends = this.friendsService.listFriends.filter((friend) =>
+      friend.username.toLowerCase().includes(usernameFriend.toLowerCase())
+    );
+    console.log(this._searchFriends);
+  }
 }
