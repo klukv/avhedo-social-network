48beb634bf02dc46e5ed377950403f212c355051
klukv <86426111+klukv@users.noreply.github.com>
2023-06-23 15:33:10 +0300

    верстка выпадающего окна результатов поиска и его реализация

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { IFriends } from 'src/app/models/friends';
 import { FriendsService } from 'src/app/services/friends.service';
 
@@ -9,13 +10,42 @@ import { FriendsService } from 'src/app/services/friends.service';
 })
 export class HeaderComponent {
   private _searchFriends: IFriends[] = [];
+  isOpenSearchPopup: boolean;
 
-  constructor(private friendsService: FriendsService) {}
+  constructor(private friendsService: FriendsService, private route: Router) {}
 
+  ngAfterViewInit(): void {
+    document.addEventListener('click', this.handlePopupSearch.bind(this));
+  }
+
+  handlePopupSearch(event: MouseEvent) {
+    const clickedElement = event.target as HTMLElement;
+
+    if (!clickedElement.closest('.header__search')) {
+      this.isOpenSearchPopup = false;
+    }
+  }
   filterSelectFriend(usernameFriend: string) {
     this._searchFriends = this.friendsService.listFriends.filter((friend) =>
       friend.username.toLowerCase().includes(usernameFriend.toLowerCase())
     );
-    console.log(this._searchFriends);
+  }
+
+  goToPageFriend(id: number){
+    this.friendsService.changeInfoFriend(id);
+    this.route.navigate(['person'], {
+      queryParams: {
+        id: id,
+      },
+    })
+    this.isOpenSearchPopup = false;
+  }
+
+  get searchFriends() {
+    return this._searchFriends;
+  }
+
+  ngOnDestroy() {
+    document.removeEventListener('click', this.handlePopupSearch.bind(this));
   }
 }
