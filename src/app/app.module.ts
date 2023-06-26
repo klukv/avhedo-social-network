daffc377edba9e31037c18b6c5ba6f4cdf7e4697
klukv <86426111+klukv@users.noreply.github.com>
2023-06-26 15:13:10 +0300

    реализация выбора даты дня рождения при помощи angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TextFieldModule } from '@angular/cdk/text-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatNativeDateModule} from '@angular/material/core';
+import { MatDatepickerModule} from '@angular/material/datepicker';
 
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SearchComponent } from './components/search/search.component';
 import { MainPageComponent } from './pages/main-page/main-page.component';
-import { AppRoutingModule } from './app-routing.module';
 import { MyPageComponent } from './components/my-page/my-page.component';
 import { FriendsBlockComponent } from './components/friends-block/friends-block.component';
 import { PostBlockComponent } from './components/post-block/post-block.component';
@@ -18,8 +24,6 @@ import { LikesBlockComponent } from './components/casting/likes-block/likes-bloc
 import { ContactsBlockComponent } from './components/casting/contacts-block/contacts-block.component';
 import { FriendsPageComponent } from './pages/friends-page/friends-page.component';
 import { PersonPageComponent } from './pages/person-page/person-page.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TextFieldModule } from '@angular/cdk/text-field';
 import { ModalComponent } from './components/modal/modal.component';
 import { CreateProductComponent } from './components/create-product/create-product.component';
 import { StopPropaginationDirective } from './directives/stop-propagination.directive';
@@ -49,13 +53,17 @@ import { FilterFriendsPipe } from './pipes/filter-friends.pipe';
     ModalComponent,
     CreateProductComponent,
     StopPropaginationDirective,
-    FilterFriendsPipe
+    FilterFriendsPipe,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    TextFieldModule
+    TextFieldModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
   ],
 
   providers: [FriendsService, PostsService],
--- src/app/components/create-product/create-product.component.ts
+++ src/app/components/create-product/create-product.component.ts
@@ -5,11 +5,25 @@ import { ModalService } from 'src/app/services/modal.service';
 @Component({
   selector: 'app-create-product',
   templateUrl: './create-product.component.html',
-  styleUrls: ['./create-product.component.css']
+  styleUrls: ['./create-product.component.css'],
 })
 export class CreateProductComponent {
   @ViewChild('autosize') autosize: CdkTextareaAutosize;
 
-  constructor(public modalService: ModalService){}
+  private _age: number;
 
+  constructor(public modalService: ModalService) {}
+
+  addEvent(selectedDate: Date) {
+    const today = new Date();
+    const birthday = new Date(selectedDate);
+    const diffMonth = today.getMonth() - birthday.getMonth();
+    this._age = today.getFullYear() - birthday.getFullYear();
+    if (diffMonth < 0 ||(diffMonth === 0 && today.getDate() < birthday.getDate())) {
+      this._age--;
+    }
+  }
+  get currentAge() {
+    return this._age;
+  }
 }
