8ea1642ae90d9e172600254227de6ca8048c425d
klukv <86426111+klukv@users.noreply.github.com>
2023-07-20 19:04:46 +0300

    реализация кнопки чата, которая появляется, если люди друг друга лайкнут

--- src/app/components/casting/likes-block/likes-block.component.ts
+++ src/app/components/casting/likes-block/likes-block.component.ts
@@ -1,4 +1,10 @@
-import { Component, ElementRef, QueryList, ViewChild, ViewChildren, Renderer2 } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  QueryList,
+  ViewChildren,
+  Renderer2,
+} from '@angular/core';
 import { CastingService } from 'src/app/services/casting.service';
 import { FriendsService } from 'src/app/services/friends.service';
 import { CASTING_PAGE, CASTING_PAGE_CONTACTS } from 'src/app/utils/const';
@@ -6,7 +12,7 @@ import { CASTING_PAGE, CASTING_PAGE_CONTACTS } from 'src/app/utils/const';
 @Component({
   selector: 'app-likes-block',
   templateUrl: './likes-block.component.html',
-  styleUrls: ['./likes-block.component.css']
+  styleUrls: ['./likes-block.component.css'],
 })
 export class LikesBlockComponent {
   @ViewChildren('like__buttons_block') likeButtonsBlock: QueryList<ElementRef>;
@@ -15,35 +21,51 @@ export class LikesBlockComponent {
 
   private _widthButtonsBlock: number;
 
-  castingLink = `/${CASTING_PAGE}/${CASTING_PAGE_CONTACTS}`
-  castingPageLink = 'castingLink'
+  castingLink = `/${CASTING_PAGE}/${CASTING_PAGE_CONTACTS}`;
+  castingPageLink = 'castingLink';
 
-  constructor(public friendsService: FriendsService, public castingService: CastingService, private render: Renderer2){
-    
-  }
+  constructor(
+    public friendsService: FriendsService,
+    public castingService: CastingService,
+    private render: Renderer2,
+  ) {}
 
-  clickLikeCard(indexCard: number){
-    if(this.likeButtonsBlock !== undefined && this.chatButton !== undefined && this.likeButtons !== undefined){
-
+  clickLikeCard(indexCard: number) {
+    if (
+      this.likeButtonsBlock !== undefined &&
+      this.chatButton !== undefined &&
+      this.likeButtons !== undefined
+    ) {
       const currentLikeButtonsBlock = this.likeButtonsBlock.toArray()[indexCard].nativeElement;
       const currentChatBlock = this.chatButton.toArray()[indexCard].nativeElement;
 
-      this._widthButtonsBlock = currentLikeButtonsBlock.offsetWidth - currentChatBlock.offsetWidth
+      this._widthButtonsBlock = currentLikeButtonsBlock.offsetWidth - currentChatBlock.offsetWidth;
 
-      this.render.setStyle(this.likeButtons.toArray()[indexCard].nativeElement, 'opacity', '0');
+      this.render.setStyle(
+        this.likeButtons.toArray()[indexCard].nativeElement,
+        'opacity',
+        '0'
+      );
 
       setTimeout(() => {
         this.render.setStyle(currentChatBlock, 'transition', '.5s');
         this.render.setStyle(currentChatBlock, 'visibility', 'visible');
         this.render.setStyle(currentChatBlock, 'opacity', '1');
         this.render.setStyle(currentChatBlock, 'z-index', '2');
-        this.render.setStyle(currentChatBlock, 'transform', `translateX(${this._widthButtonsBlock}px)`);
+        this.render.setStyle(
+          currentChatBlock,
+          'transform',
+          `translateX(${this._widthButtonsBlock}px)`
+        );
       }, 200);
     }
   }
- 
-  clickDislikeCard(indexCard: number){
+
+  clickDislikeCard(indexCard: number) {
     this.castingService.deleteUnlikeCard(indexCard);
   }
 
+  clickChatButton(idLikePerson: number) {
+    this.friendsService.goToChat(idLikePerson);
+  }
 }
